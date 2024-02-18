e4cc301af1cca3682cc66cabcb6a933682fb682d
dedenlpg <kangdedenonline@gmail.com>
2024-02-18 23:43:26 +0700

    auttentikasi view

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,20 @@
 <?php
 
+
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::controller(HomeController::class)->group(function () {
     Route::get('/', 'index');
 });
+
+Route::controller(LoginController::class)->group(function () {
+    Route::get('/login', 'login')->name('loginIndex');
+    Route::get('/daftar', 'daftar')->name('daftarIndex');
+});
+
+
+
+
